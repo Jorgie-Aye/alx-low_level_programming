99a9be2c3c52b5a9544d2af10d05880783243902
Jorgie-Aye <gehorgebushiton21@gmail.com>
2022-09-08 22:40:26 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - print out the string in the printf function
-* 
-* Description: prints out the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Retrun: 0
-*/
+ * main - print the entry
+ * 
+ * Description: prints out the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Retrun: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
