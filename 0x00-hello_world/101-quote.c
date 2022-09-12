b05587231f4c313e65bcd70039abac43c989ae3c
Jorgie-Aye <gehorgebushiton21@gmail.com>
2022-09-12 00:22:49 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ *main - print a text
+ *
+ *Return: 1 (successful)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,8 @@
 
 /**
  * main - Entry point
- * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char a;
@@ -14,10 +12,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of char: %d byte(s)\n",sizeof(a));
-	printf("Size of int: %d byte(s)\n",sizeof(b));
-	printf("Size of long int: %d byte(s)\n",sizeof(c));
-	printf("Size of long long int: %d byte(s)\n",sizeof(d));
-	printf("Size of float: %d byte(s)\n",sizeof(e));
-        return (0);
+	printf("Size of a char: %d byte(s)\n," sizeof(a));
+	printf("Size of an int: %d byte(s)\n," sizeof(b));
+	printf("Size of a long int: %d byte(s)\n," sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n," sizeof(d));
+	printf("Size of a float: : %d byte(s)\n," sizeof(e)); 
+	return (0);
 }
